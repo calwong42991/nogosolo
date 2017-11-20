ecb062a69dc3860f6f8e85ac25926ed4be083972
Calvin Wong <calwong42991@gmail.com>
2017-11-20 15:00:14 -0800

    modified searchbox

--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
+import apiKey from '../../../config';
+
 
 class Search extends React.Component {
   constructor(props) {
@@ -22,9 +24,8 @@ class Search extends React.Component {
     $.ajax({
       type: 'GET',
       datatype: 'json',
-      url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=yGpWxZKlGoITHFBV1OXbAoiAL6skfN20&keyword=${this.state.keyword}`,
+      url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&keyword=${this.state.keyword}`,
       success: (data) => {
-        console.log('data', data);
         const eventNames = data._embedded.events.map(event => event.name);
         this.setState({
           results: eventNames,
@@ -34,20 +35,13 @@ class Search extends React.Component {
   }
 
   render() {
-
     return (
       <div>
-        <input
-          type="text"
-          value={this.props.keyword}
-          onChange={this.handleChange}
-          placerholder="Seach for an Event"/>
+        <input type="text" value={this.state.keyword} onChange={this.handleChange} />
         <button onClick={this.handleClick}>search</button>
         <h3>Search Results for: {this.state.keyword} </h3>
         There Are {this.state.results.length} event results.
-        {this.state.results.map((eventNames) => {
-          return <li>{eventNames}</li>
-        })}
+        {this.state.results.map((eventNames) => { return <li>{eventNames}</li>; })}
       </div>
     );
   }
